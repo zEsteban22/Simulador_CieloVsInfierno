83a9dd39aafbfbcb2bd6cafb3fb587ef067d3745
Esteban Cruz <xllestebanllx@gmail.com>
2019-05-29 12:18:30 -0600

    .

--- Simulador_CieloVsInfierno.pro
+++ Simulador_CieloVsInfierno.pro
@@ -74,3 +74,14 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
   continentescondegradacion.qrc
+
+# Location of SMTP Library
+S#MTP_LIBRARY_LOCATION = $$PWD/../build-SMTPEmail-Desktop-Debug
+
+#win32:CONFIG(release, debug|release): LIBS += -L$$SMTP_LIBRARY_LOCATION/release/ -lSMTPEmail
+#else:win32:CONFIG(debug, debug|release): LIBS += -L$$SMTP_LIBRARY_LOCATION/debug/ -lSMTPEmail
+#else:unix: LIBS += -L$$SMTP_LIBRARY_LOCATION -lSMTPEmail
+
+INCLUDEPATH += $$SMTP_LIBRARY_LOCATION
+DEPENDPATH += $$SMTP_LIBRARY_LOCATION
+
--- controller.cpp
+++ controller.cpp
@@ -1,5 +1,40 @@
 #include "controller.h"
 
+void Controller::enviarCorreo(string mensaje){
+	SmtpClient smtp("smtp.gmail.com", 587, SmtpClient::TlsConnection);
+
+	smtp.setUser("cuentanivelbajo@gmail.com");
+	smtp.setPassword("esteBan2203");
+
+	MimeMessage message;
+
+	EmailAddress sender("cuentanivelbajo@gmail.com", "Yo:3");
+	message.setSender(&sender);
+
+	EmailAddress to("xllestebanllx@gmail.com", "Primoxd");
+	message.addRecipient(&to);
+
+	message.setSubject("Log");
+
+	MimeText text;
+
+	text.setText(QString::fromStdString(mensaje));
+
+	message.addPart(&text);
+
+	if (!smtp.connectToHost())
+		QMessageBox(QMessageBox::Critical,QString("Error"),
+								QString("No se ha podido conectar al host"),QMessageBox::Ok).exec();
+	if (!smtp.login())
+		QMessageBox(QMessageBox::Critical,QString("Error"),
+								QString("No se ha podido iniciar sesión"),QMessageBox::Ok).exec();
+	if (!smtp.sendMail(message))
+		QMessageBox(QMessageBox::Critical,QString("Error"),
+								QString("No se ha podido enviar el correo"),QMessageBox::Ok).exec();
+
+	smtp.quit();
+}
+
 Controller::Controller()
 {
 
